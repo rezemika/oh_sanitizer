e25d8387afc2d13014eda96f15c11bd58bebdc87
Michael Marx <michael-marx@protonmail.ch>
2019-01-24 15:19:17 +0100

    Updates version to 0.1.10

--- oh_sanitizer/__init__.py
+++ oh_sanitizer/__init__.py
@@ -18,7 +18,7 @@ Example:
 "Mo-Fr 10:00-19:00"
 """
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 __appname__ = "oh_sanitizer"
 __author__ = "rezemika <reze.mika@gmail.com>"
 __licence__ = "AGPLv3"
